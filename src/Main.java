4eeb524824a6560c8ccce3d7892ee36e9770721a
hyuk <tig05205@naver.com>
2024-08-16 20:44:47 +0900

    Observer Pattern

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import adapter.*;
 import aop.AopBrowser;
 import decorator.*;
+import observer.Button;
+import observer.IButtonListener;
 import proxy.Browser;
 import proxy.BrowserProxy;
 import proxy.IBrowser;
@@ -61,18 +63,31 @@ public class Main {
 //        System.out.println("loading time : " + end.get());
 //        aopBrowser.show();
 //        System.out.println("loading time : " + end.get());
-        ICar audi = new Audi(1000);
-        audi.showPrice();
-        //a3
-        ICar audi3 = new A3(audi, "A3");
-        audi3.showPrice();
-        //a4
-        ICar audi4 = new A4(audi, "A4");
-        audi4.showPrice();
-        //a5
-        ICar audi5 = new A5(audi, "A5");
-        audi5.showPrice();
+//        ICar audi = new Audi(1000);
+//        audi.showPrice();
+//        //a3
+//        ICar audi3 = new A3(audi, "A3");
+//        audi3.showPrice();
+//        //a4
+//        ICar audi4 = new A4(audi, "A4");
+//        audi4.showPrice();
+//        //a5
+//        ICar audi5 = new A5(audi, "A5");
+//        audi5.showPrice();
 
+        Button button = new Button("버튼");
+
+        button.addListener(new IButtonListener() {
+            @Override
+            public void clickEvent(String event) {
+                System.out.println(event);
+            }
+        });
+
+        button.click("메시지 전달 : click 1");
+        button.click("메시지 전달 : click 2");
+        button.click("메시지 전달 : click 3");
+        button.click("메시지 전달 : click 4");
     }
 
 
